c780612c9f545b8293d285ff603a45f3e1cd8d54
wrongwrong <boranti1995@gmail.com>
2022-04-02 04:50:26 +0900

    fix extraArgumentsTail

--- core/reflection.jvm/src/kotlin/reflect/jvm/internal/calls/InlineClassAwareCaller.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/internal/calls/InlineClassAwareCaller.kt
@@ -75,9 +75,6 @@ internal class InlineClassAwareCaller<out M : Member?>(
             else -> 0
         }
 
-        val extraArgumentsTail = (if (isDefault) 2 else 0) +
-                (if (descriptor is FunctionDescriptor && descriptor.isSuspend) 1 else 0)
-
         val kotlinParameterTypes: List<KotlinType> = ArrayList<KotlinType>().also { kotlinParameterTypes ->
             val extensionReceiverType = descriptor.extensionReceiverParameter?.type
             if (extensionReceiverType != null) {
@@ -96,6 +93,10 @@ internal class InlineClassAwareCaller<out M : Member?>(
 
             descriptor.valueParameters.mapTo(kotlinParameterTypes, ValueParameterDescriptor::getType)
         }
+
+        // If the default argument is set, ((paramSize / Int.SIZE_BITS) + 1) masks and one marker are added to the end of the argument.
+        val extraArgumentsTail = (if (isDefault) (kotlinParameterTypes.size / Int.SIZE_BITS) + 2 else 0) +
+                (if (descriptor is FunctionDescriptor && descriptor.isSuspend) 1 else 0)
         val expectedArgsSize = kotlinParameterTypes.size + shift + extraArgumentsTail
         if (arity != expectedArgsSize) {
             throw KotlinReflectionInternalError(
